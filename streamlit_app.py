598b4a444ada30ed278e9a5bb4a19417a6b64815
nabendu-bhuiya <nabendu.bhuiya@gmail.com>
2023-03-24 20:17:11 +0530

    Update streamlit_app.py
    updated menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,9 @@ import streamlit
 streamlit.title("My Parents new Healthy Dinner !")
 
 streamlit.header("Breakfast Menu")
-streamlit.text("Omega 3 and Blueberry Oatmeal")
-streamlit.text("Kale, Spinach and Rocket Smotthie")
-streamlit.text("Hard Boiled Free-Range Egg")
+streamlit.text("🥣Omega 3 and Blueberry Oatmeal")
+streamlit.text("🥗 Kale, Spinach and Rocket Smotthie")
+streamlit.text("🐔Hard Boiled Free-Range Egg")
+streamlit.text("🥑🍞avacado toast")
 
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
